a01c3deb462c355cba13c9e174d98913ca5baf26
dungquixote42 <hyunsoo.david.kim@gmail.com>
2023-12-05 00:36:46 -0500

    add separate methods for reshaping input and output, add missing files handler, remove old comments

--- NeurosismDataset.py
+++ NeurosismDataset.py
@@ -4,101 +4,90 @@ import os
 
 import torch
 from torch.utils.data import Dataset
+# from torch import Tensor
 
 
-DEFAULT_FRAME_SKIP_COUNT = 50
-POOL_FACTOR = 32
+FRAME_SKIP_COUNT = 50
+# POOL_FACTOR = 8
+POOL_FACTOR = 16  # 5528
+# POOL_FACTOR = 32  # 2496
+# POOL_FACTOR = 64 # 394
+
+
+def get_valid_files(filePath1: str, filePath2: str) -> list:
+    fileNames1 = os.listdir(filePath1)
+    fileNames2 = os.listdir(filePath2)
+    result = []
+    for f in fileNames1:
+        if f in fileNames2:
+            result.append(f)
+    return result
 
 
 def reshape_video_hwd_to_dhw(video: np.ndarray) -> np.ndarray:
     (height, width, duration) = video.shape
-    result = np.empty((duration, height, width))
-
     indices = [(d, h, w) for d in range(duration) for h in range(height) for w in range(width)]
+    result = np.zeros((duration, height, width))
     for d, h, w in indices:
         result[d][h][w] = video[h][w][d]
-
     return result
 
 
 class NeurosismDataset(Dataset):
-    def __init__(self, frameCount, parentDirectory, transform=None, transformTarget=None):
-        self.frameCount = frameCount
-        self.coordinates = np.load(parentDirectory + "/meta/neurons/cell_motor_coordinates.npy")
+    def __init__(self, device, frameCount, parentDirectory, transform=None, transform_target=None):
+        self.device = device
+        self.frameCountActual = frameCount - FRAME_SKIP_COUNT
+        self.coordinates: np.ndarray = np.load(parentDirectory + "/meta/neurons/cell_motor_coordinates.npy")
         self.responsePath = parentDirectory + "/data/responses/"
         self.videoPath = parentDirectory + "/data/videos/"
+
+        self.neuronCount = self.coordinates.shape[0]
         self._generate_data_dimensions()
+        self.validFiles = get_valid_files(self.responsePath, self.videoPath)
 
     def __len__(self):
-        return len(os.listdir(self.videoPath))
+        return len(self.validFiles)
 
     def __getitem__(self, idx):
-        data = torch.zeros(self.frameCount - DEFAULT_FRAME_SKIP_COUNT, self.xRange, self.yRange, self.zRange)
-        fileName = str(idx) + ".npy"
-        responses = torch.tensor(np.load(self.responsePath + fileName))
+        fileName = self.validFiles[idx]
+
+        responses = np.load(self.responsePath + fileName)
+        video = np.load(self.videoPath + fileName)
+
+        data = np.zeros((self.frameCountActual, self.xRange, self.yRange, self.zRange))
         for n in range(self.neuronCount):
             (x, y, z) = self.coordinates[n]
             x = (x - self.xMin) // POOL_FACTOR
             y = (y - self.yMin) // POOL_FACTOR
             z = (z - self.zMin) // POOL_FACTOR
             responsesOverTime = responses[n]
-            for t in range(self.frameCount - DEFAULT_FRAME_SKIP_COUNT):
-                data[t][x][y][z] += responsesOverTime[t + DEFAULT_FRAME_SKIP_COUNT]
-                # currentData = data[t][x][y][z]
-                # incomingResponse = responsesOverTime[t + DEFAULT_FRAME_SKIP_COUNT]
-                # # if n == 0:
-                # #     print(incomingResponse)
-                # if incomingResponse > currentData:
-                #     # if n == 0:
-                #     #     print("test")
-                #     data[t][x][y][z] = incomingResponse
-        # for t in range(self.frameCount - DEFAULT_FRAME_SKIP_COUNT):
-        #     currentSlice = data[t]
-        #     # incomingSlice = responses[t + DEFAULT_FRAME_SKIP_COUNT]
-        #     for n in range(self.neuronCount):
-        #         (x, y, z) = self.coordinates[n]
-        #         x -= self.xMin
-        #         y -= self.yMin
-        #         z -= self.zMin
-        #         xPool = x // POOL_FACTOR
-        #         yPool = y // POOL_FACTOR
-        #         zPool = z // POOL_FACTOR
-        #         currentData = currentSlice[xPool][yPool][zPool]
-        #         incomingData = incomingSlice[x][y][z]
-        #         if incomingData > currentData:
-        #             data[t][xPool][yPool][zPool] = incomingData
-        video = reshape_video_hwd_to_dhw(np.load(self.videoPath + fileName))
-        # data = self.dataTemplate.copy()
-        # for n in range(self.neuronCount):
-        #     (x, y, z) = self.coordinates[n]
-        #     responsesOverTime = responses[n]
-        #     for t in range(len(responsesOverTime) - DEFAULT_FRAME_SKIP_COUNT):
-        #         data[t][x - self.xMin][y - self.yMin][z - self.zMin] = responsesOverTime[t + DEFAULT_FRAME_SKIP_COUNT]
-        return data, torch.tensor(video)
+            for t in range(self.frameCountActual):
+                current = data[t][x][y][z]
+                incoming = responsesOverTime[t + FRAME_SKIP_COUNT]
+                data[t][x][y][z] = incoming if incoming > current else current
+                # data[t][x][y][z] += responsesOverTime[t + FRAME_SKIP_COUNT]
+        # video = reshape_video_hwd_to_dhw(np.load(self.videoPath + fileName))
+        data = self._reshape_data(data)
+        video = self._reshape_video(video)
 
-    def _generate_data_template(self, frameCount: int, frameSkipCount=DEFAULT_FRAME_SKIP_COUNT):
-        self.xMin = xMax = 0
-        self.yMin = yMax = 0
-        self.zMin = zMax = 0
-        self.neuronCount = self.coordinates.shape[0]
-        for n in range(self.neuronCount):
-            (x, y, z) = self.coordinates[n]
-            xMax = x if x > xMax else xMax
-            yMax = y if y > yMax else yMax
-            zMax = z if z > zMax else zMax
-            self.xMin = x if x < self.xMin else self.xMin
-            self.yMin = y if y < self.yMin else self.yMin
-            self.zMin = z if z < self.zMin else self.zMin
-        xRange = xMax - self.xMin
-        yRange = yMax - self.yMin
-        zRange = zMax - self.zMin
-        self.dataTemplate = torch.zeros(frameCount - frameSkipCount, xRange, yRange, zRange)
+        return data, video
+
+    # def _fill_data(self, data, n):
+    #     # for n in range(self.neuronCount):
+    #     (x, y, z) = self.coordinates[n]
+    #     x = (x - self.xMin) // POOL_FACTOR
+    #     y = (y - self.yMin) // POOL_FACTOR
+    #     z = (z - self.zMin) // POOL_FACTOR
+    #     responsesOverTime = responses[n]
+    #     for t in range(self.frameCountActual):
+    #         current = data[t][x][y][z]
+    #         incoming = responsesOverTime[t + FRAME_SKIP_COUNT]
+    #         data[t][x][y][z] = incoming if incoming > current else current
 
     def _generate_data_dimensions(self):
         self.xMin = xMax = 0
         self.yMin = yMax = 0
         self.zMin = zMax = 0
-        self.neuronCount = self.coordinates.shape[0]
         for n in range(self.neuronCount):
             (x, y, z) = self.coordinates[n]
             xMax = x if x > xMax else xMax
@@ -110,3 +99,27 @@ class NeurosismDataset(Dataset):
         self.xRange = 1 + (xMax - self.xMin) // POOL_FACTOR
         self.yRange = 1 + (yMax - self.yMin) // POOL_FACTOR
         self.zRange = 1 + (zMax - self.zMin) // POOL_FACTOR
+
+    def _reshape_data(self, data: np.ndarray) -> np.ndarray:
+        (time, x, y, z) = data.shape
+        result = np.zeros((z, x * y, time))
+        indices = [(t, xx, yy, zz) for t in range(time) for xx in range(x) for yy in range(y) for zz in range(z)]
+        for t, xx, yy, zz in indices:
+            result[zz][y * xx + yy][t] = data[t][xx][yy][zz]
+        return result
+        # big = max(x, y, z)
+        # large = int(x * y * z / big)
+        # data = data.reshape((t, min(big, large), max(big, large)))
+        # data = data.reshape(1, t, x * y * z)
+        # if self.device == "cuda":
+        #     return data.cuda()
+        # return data
+
+    def _reshape_video(self, video: np.ndarray) -> np.ndarray:
+        (height, width, time) = video.shape
+        assert self.frameCountActual <= time
+        result = np.zeros((1, height * width, self.frameCountActual))
+        indices = [(t, h, w) for t in range(self.frameCountActual) for h in range(height) for w in range(width)]
+        for t, h, w in indices:
+            result[0][width * h + w][t] = video[h][w][t + FRAME_SKIP_COUNT]
+        return result
